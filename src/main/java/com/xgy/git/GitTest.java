67971417287208ac3abfdb7f74088762d7a1579e
xgy <779323365@qq.com>
2022-07-24 19:44:07 +0800

    push commit

--- src/main/java/com/xgy/git/GitTest.java
+++ src/main/java/com/xgy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master test 000");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
